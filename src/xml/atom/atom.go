f54f6e06df108336fa34d84bbdb34cab01233cac
info-matopush <info.matopush@gmail.com>
2018-05-02 13:34:21 +0900

    Qiita用にatom解析ロジックを修正

--- src/xml/atom/atom.go
+++ src/xml/atom/atom.go
@@ -18,6 +18,7 @@ type entry struct {
 	Title    string `xml:"title"`
 	Link     []link `xml:"link"`
 	Modified string `xml:"modified"`
+	Updated  string `xml:"updated"`
 	Summary  string `xml:"summary"`
 }
 
@@ -55,10 +56,14 @@ func Analyze(bytes []byte) (content.Feed, error) {
 			Summary: item.Summary,
 		}
 
-		// 2018-03-31T10:02:32+09:00
+		// 2018-03-31T10:02:32Z MST
 		layout1 := "2006-01-02T15:04:05Z MST"
+		layout2 := "2006-01-02T15:04:05-07:00"
 		cff.ModifyDate, err = time.Parse(layout1, item.Modified+" JST")
-		// エラーは無視する
+		if err != nil {
+			cff.ModifyDate, err = time.Parse(layout2, item.Updated)
+			// エラーは無視する
+		}
 
 		feed.Contents = append(feed.Contents, cff)
 	}
