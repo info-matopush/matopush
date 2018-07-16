ca9eeb9f37c36a306af6883378ad2cb4b79a9015
info-matopush <info.matopush@gmail.com>
2018-07-16 13:09:22 +0900

    inviteのパラメータを修正

--- doc/sitemap.go
+++ doc/sitemap.go
@@ -3,6 +3,7 @@ package doc
 import (
 	"encoding/xml"
 	"net/http"
+	"strings"
 
 	"github.com/info-matopush/matopush/site"
 	"github.com/info-matopush/matopush/xml/sitemap"
@@ -12,15 +13,22 @@ import (
 // SitemapHandler はsitemap.xmlを生成する
 func SitemapHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
+
+	mainURL := "https://matopush.appspot.com/"
+	mainURL = strings.Replace(mainURL, "matopush", appengine.AppID(ctx), -1)
+
 	list := site.PublicList(ctx)
 
 	w.Header().Set("content-type", "application/xml")
 	w.Write([]byte(xml.Header))
-	s := sitemap.FromLocation("https://matopush.appspot.com/")
+	s := sitemap.FromLocation(mainURL)
 
 	for _, site := range list {
-		u := "https://matopush.appspot.com/invite?FeedURL=" + site.FeedURL
-		s.Append(sitemap.CreateURLFromLocation(u))
+		req, _ := http.NewRequest("GET", mainURL+"invite", nil)
+		q := req.URL.Query()
+		q.Add("FeedURL", site.FeedURL)
+		req.URL.RawQuery = q.Encode()
+		s.Append(sitemap.CreateURLFromLocation(req.URL.String()))
 	}
 
 	buf, _ := xml.MarshalIndent(s, "", "  ")
